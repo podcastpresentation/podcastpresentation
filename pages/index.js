1337a1e3ad11a257c8217b93e5449927f4b05302
krazykoolkat <120543332+podcastpresentation@users.noreply.github.com>
2023-05-12 13:10:36 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Podcast Presentation</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my Podcast" />
         <p className="This is my Passion Project Podcast About Fishing">
-          Get started by editing <code>pages/index.js</code>
         </p>
       </main>
 
